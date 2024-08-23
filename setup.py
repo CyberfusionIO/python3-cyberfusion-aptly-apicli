d85bde5a9ac2f0f7ec1d49257e6e03512a44232e
William Edwards <wedwards@cyberfusion.nl>
2024-08-23 10:14:59 +0200

    Create /etc/cyberfusion (dh_make no longer does with 639a8e48b0be90dd35f73eb2220dfde9c9b22e85)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="python3-cyberfusion-aptly-apicli",
-    version="1.0.2.2",
+    version="1.0.2.2.1",
     description="API library for Aptly.",
     long_description=long_description,
     long_description_content_type="text/markdown",
